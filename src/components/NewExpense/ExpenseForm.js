fa721ed6fa80a595afb43d5d061259a3a7873c85
trandainien <tdnien21@clc.fitus.edu.vn>
2022-03-21 19:41:30 +0700

    pass a function from parent to children

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [amount, setAmount] = useState("");
   const [date, setDate] = useState("");
@@ -40,6 +40,8 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+
+    props.onSaveExpenseData(expenseData);
 
     setEnteredTitle("");
     setAmount("");
