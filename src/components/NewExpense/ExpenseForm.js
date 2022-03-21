62ec3b6f4abf0aa564ac0cb99f8661229c0d580f
trandainien <tdnien21@clc.fitus.edu.vn>
2022-03-21 19:16:31 +0700

    two way binding
    adding two way binding by add value atributes and set its state to empty

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,44 +1,60 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [amount, setAmount] = useState("");
-  //   const [date, setDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    amount: "",
-    date: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     amount: "",
+  //     date: "",
+  //   });
+
+  //   const titleChangeHandler = (e) => {
+  //     setUserInput((prevState) => {
+  //       return {
+  //         ...prevState, //pull out all key value pair and add it to new object
+  //         enteredTitle: e.target.value,
+  //       };
+  //     });
+  //   };
 
   const titleChangeHandler = (e) => {
-    setUserInput({
-      ...userInput, //pull out all key value pair and add it to new object
-      enteredTitle: e.target.value,
-    });
+    setEnteredTitle(e.target.value);
   };
 
   const amountChangeHandler = (e) => {
-    setUserInput({
-      ...userInput,
-      amount: e.target.value,
-    });
+    setAmount(e.target.value);
   };
 
   const dateChangeHandler = (e) => {
-    setUserInput({
-      ...userInput,
-      date: e.target.value,
-    });
+    setDate(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: amount,
+      date: new Date(date),
+    };
+
+    console.log(expenseData);
+
+    setEnteredTitle("");
+    setAmount("");
+    setDate("");
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="row">
         <div className="input-box">
           <label>Title</label>
           <input
             type="text"
             className="form-control"
+            value={enteredTitle}
             onChange={titleChangeHandler}
           />
         </div>
@@ -49,6 +65,7 @@ const ExpenseForm = () => {
             min="0.01"
             step="0.01"
             className="form-control"
+            value={amount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -59,6 +76,7 @@ const ExpenseForm = () => {
             min="2019-01-01"
             max="2022-12-31"
             className="form-control"
+            value={date}
             onChange={dateChangeHandler}
           />
         </div>
