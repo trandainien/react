eb8c3693f3718ac1007a25f2230ef197def56de4
trandainien <tdnien21@clc.fitus.edu.vn>
2022-03-21 07:48:44 +0700

    useState
    use useState to take the change in the input, instead of using mutilple states, we can use an object and when setState remmember to use spread operator

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
+  //   const [enteredTitle, setEnteredTitle] = useState("");
+  //   const [amount, setAmount] = useState("");
+  //   const [date, setDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    amount: "",
+    date: "",
+  });
+
+  const titleChangeHandler = (e) => {
+    setUserInput({
+      ...userInput, //pull out all key value pair and add it to new object
+      enteredTitle: e.target.value,
+    });
+  };
+
+  const amountChangeHandler = (e) => {
+    setUserInput({
+      ...userInput,
+      amount: e.target.value,
+    });
+  };
+
+  const dateChangeHandler = (e) => {
+    setUserInput({
+      ...userInput,
+      date: e.target.value,
+    });
+  };
+
   return (
     <form>
       <div className="row">
         <div className="input-box">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="input-box">
           <label>Amount</label>
@@ -15,6 +49,7 @@ const ExpenseForm = () => {
             min="0.01"
             step="0.01"
             className="form-control"
+            onChange={amountChangeHandler}
           />
         </div>
         <div className="input-box">
@@ -24,6 +59,7 @@ const ExpenseForm = () => {
             min="2019-01-01"
             max="2022-12-31"
             className="form-control"
+            onChange={dateChangeHandler}
           />
         </div>
       </div>
